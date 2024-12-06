42901067ebf7dc3634ce7d28cd7c81e1ad0a370e
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-12-06 16:23:15 +0400

    Update greeting message in app.js to reflect new content

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Salam bu ise teze!',
+    message: 'Ha gau=yret!',
     hostname: os.hostname(),
     timestamp: new Date().toISOString()
   });
